6f0c60a2cd75bf6a77d015c65b8b01bc0dcdb8b6
YunussEmree <86290901+YunussEmree@users.noreply.github.com>
2022-07-16 21:28:40 +0300

    Add files via upload

---
+++ RewardSystemjava/src/main/java/provanasservices/rewardsystem/Events.java
@@ -0,0 +1,295 @@
+package provanasservices.rewardsystem;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static provanasservices.rewardsystem.Main.*;
+
+public class Events implements Listener {
+    Main plugin;
+    public Events(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event) {
+        Entity entity = event.getEntity();
+        Entity damager = event.getDamager();
+        if ((damager instanceof Player) || (entity instanceof Mob)) {
+            Mob mob = (Mob) entity;
+            Player player = (Player) damager;
+            RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+
+
+            for (int i = 0; i < rewardsFromConfig.size(); i++) {
+
+                RewardMob reward = rewardsFromConfig.get(i);
+                if (reward.nameEquals(entity.getName()) && reward.typeEquals(entity.getType().name()) && reward.worldEquals(entity.getWorld().getName())) {
+
+                    if (reward.getEnabledWorld() != null) {
+                        World world = Bukkit.getWorld(reward.getEnabledWorld());
+                        assert world != null;
+                        if (reward.getEnabledRegion() != null) {
+
+                            RegionManager regions = container.get(BukkitAdapter.adapt(world));
+                            ProtectedRegion region = regions.getRegion(reward.getEnabledRegion());
+                            if (region.contains(BukkitAdapter.asBlockVector(entity.getLocation()))) {
+
+                                addToDamageMap(event, player, i);
+                            }
+                        } else {
+                            ;
+                            addToDamageMap(event, player, i);
+                        }
+                    } else {
+
+                        addToDamageMap(event, player, i);
+                    }
+
+                }
+            }
+
+
+            if (mob.getHealth() - event.getFinalDamage() <= 0) {
+                for (int i = 0; i < rewardsFromConfig.size(); i++) {
+
+                    RewardMob reward = rewardsFromConfig.get(i);
+                    if (reward.nameEquals(entity.getName()) && reward.typeEquals(entity.getType().name()) && reward.worldEquals(entity.getWorld().getName())) {
+
+                        if (reward.getEnabledWorld() != null) {
+                            World world = Bukkit.getWorld(reward.getEnabledWorld());
+                            assert world != null;
+                            if (reward.getEnabledRegion() != null) {
+
+                                RegionManager regions = container.get(BukkitAdapter.adapt(world));
+                                ProtectedRegion region = regions.getRegion(reward.getEnabledRegion());
+                                if (region.contains(BukkitAdapter.asBlockVector(entity.getLocation()))) {
+
+                                    giveRewards(i, reward);
+                                }
+
+                            } else {
+
+                                giveRewards(i, reward);
+                            }
+                        } else {
+
+                            giveRewards(i, reward);
+                        }
+                        HashMap<String, Double> selectedMap = damageMap.get(i + 1);
+                        if (reward.getRadius() == -1) {
+                            reward.getRewardMessages().forEach(message -> {
+                                Bukkit.getOnlinePlayers().forEach(onlinePlayer -> onlinePlayer.sendMessage(translateColors(replacePlaceholders(message, player.getName(), selectedMap))));
+                            });
+                        } else {
+                            entity.getNearbyEntities(reward.getRadius(), reward.getRadius(), reward.getRadius()).forEach(e -> {
+                                if (e instanceof Player) {
+                                    Player p = (Player) e;
+                                    reward.getRewardMessages().forEach(message -> {
+                                        p.sendMessage(translateColors(replacePlaceholders(message, player.getName(), selectedMap)));
+                                    });
+                                }
+                            });
+                        }
+                        damageMap.get(i + 1).clear();
+                    }
+                }
+            }
+        }
+
+        else if(damager instanceof Arrow){
+            Projectile proj = (Projectile) event.getDamager();
+            Player player = (Player) proj.getShooter();
+
+            Mob mob = (Mob) entity;
+            RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+
+
+            for (int i = 0; i < rewardsFromConfig.size(); i++) {
+
+                RewardMob reward = rewardsFromConfig.get(i);
+                if (reward.nameEquals(entity.getName()) && reward.typeEquals(entity.getType().name()) && reward.worldEquals(entity.getWorld().getName())) {
+
+                    if (reward.getEnabledWorld() != null) {
+                        World world = Bukkit.getWorld(reward.getEnabledWorld());
+                        assert world != null;
+                        if (reward.getEnabledRegion() != null) {
+
+                            RegionManager regions = container.get(BukkitAdapter.adapt(world));
+                            ProtectedRegion region = regions.getRegion(reward.getEnabledRegion());
+                            if (region.contains(BukkitAdapter.asBlockVector(entity.getLocation()))) {
+
+                                addToDamageMap(event, player, i);
+                            }
+                        } else {
+                            ;
+                            addToDamageMap(event, player, i);
+                        }
+                    } else {
+
+                        addToDamageMap(event, player, i);
+                    }
+
+                }
+            }
+
+
+            if (mob.getHealth() - event.getFinalDamage() <= 0) {
+                for (int i = 0; i < rewardsFromConfig.size(); i++) {
+
+                    RewardMob reward = rewardsFromConfig.get(i);
+                    if (reward.nameEquals(entity.getName()) && reward.typeEquals(entity.getType().name()) && reward.worldEquals(entity.getWorld().getName())) {
+
+                        if (reward.getEnabledWorld() != null) {
+                            World world = Bukkit.getWorld(reward.getEnabledWorld());
+                            assert world != null;
+                            if (reward.getEnabledRegion() != null) {
+
+                                RegionManager regions = container.get(BukkitAdapter.adapt(world));
+                                ProtectedRegion region = regions.getRegion(reward.getEnabledRegion());
+                                if (region.contains(BukkitAdapter.asBlockVector(entity.getLocation()))) {
+
+                                    giveRewards(i, reward);
+                                }
+
+                            } else {
+
+                                giveRewards(i, reward);
+                            }
+                        } else {
+
+                            giveRewards(i, reward);
+                        }
+                        HashMap<String, Double> selectedMap = damageMap.get(i + 1);
+                        if (reward.getRadius() == -1) {
+                            reward.getRewardMessages().forEach(message -> {
+                                Bukkit.getOnlinePlayers().forEach(onlinePlayer -> onlinePlayer.sendMessage(translateColors(replacePlaceholders(message, player.getName(), selectedMap))));
+                            });
+                        } else {
+                            entity.getNearbyEntities(reward.getRadius(), reward.getRadius(), reward.getRadius()).forEach(e -> {
+                                if (e instanceof Player) {
+                                    Player p = (Player) e;
+                                    reward.getRewardMessages().forEach(message -> {
+                                        p.sendMessage(translateColors(replacePlaceholders(message, player.getName(), selectedMap)));
+                                    });
+                                }
+                            });
+                        }
+                        damageMap.get(i + 1).clear();
+                    }
+                }
+            }
+        } else {
+            return;
+        }
+    }
+
+    private void giveRewards(int i, RewardMob reward) {
+        HashMap<String, Double> selectedDamageMap = damageMap.get(i+1);
+        ArrayList<Map.Entry<String, Double>> entrySet = new ArrayList<>(selectedDamageMap.entrySet());
+        entrySet.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+        for(int index = 0; index < selectedDamageMap.size(); index++){
+            int rewardIndex = index + 1;
+            Map.Entry<String, Double> entry = entrySet.get(index);
+            reward.getAllRewards().forEach(allReward -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), allReward.replace("%player%", entry.getKey()).replace("%damage%", entry.getValue().toString())));
+            reward.getRewards().get(rewardIndex).forEach(rewardString -> {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), rewardString.replace("%player%", entry.getKey()).replace("%damage%", String.valueOf(entry.getValue())));
+            });
+        }
+    }
+
+    private void addToDamageMap(EntityDamageByEntityEvent event, Player player, int i) {
+        HashMap<String, Double> selectedDamageMap = damageMap.get(i+1);
+        Double selectedDamage = selectedDamageMap.get(player.getName());
+        if(selectedDamage == null) selectedDamage = 0.0;
+        selectedDamage += event.getDamage();
+        selectedDamageMap.put(player.getName(), selectedDamage);
+        selectedDamageMap.put("Deneme", 5.0);
+    }
+
+    private static String replacePlaceholders(String string, String playerName,  HashMap<String, Double> map){
+        ArrayList<Map.Entry<String, Double>> entries =new ArrayList<>(map.entrySet());
+        entries.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+        System.out.println(entries);
+        Pattern pattern = Pattern.compile("%damager_(\\d+)%");
+        Matcher matcher = pattern.matcher(string);
+        String newStr = string;
+        while(matcher.find()){
+            System.out.println(matcher.group(1));
+            int index = Integer.parseInt(matcher.group(1))-1;
+            if(index < entries.size()){
+                newStr = newStr.replace(matcher.group(), entries.get(index).getKey());
+            }
+        }
+        newStr = newStr.replace("%personal_damage%", String.valueOf(map.get(playerName)));
+        return newStr;
+    }
+
+    @EventHandler
+    public void debug(EntityDamageByEntityEvent event){
+
+        Entity entity = event.getEntity();
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            if(player.isOp()){
+                World world = entity.getLocation().getWorld();
+                String type = entity.getType().getName();
+                String name = entity.getName();
+
+                player.sendMessage("§a§l-------[REWARD SYSTEM DEBUG MESSAGE]-------");
+                player.sendMessage(ChatColor.AQUA + "if you wont see this message, you should set debug: false in config.yml");
+                player.sendMessage(ChatColor.AQUA + " ");
+                player.sendMessage(ChatColor.BLUE + "Mob world: " + world);
+                player.sendMessage(ChatColor.BLUE + "Mob type: " + type);
+                player.sendMessage(ChatColor.BLUE + "Mob name: " + name);
+                player.sendMessage("§a§l------------------------------------------");
+
+            }
+        }
+    }
+
+    @EventHandler
+    public void reload(PlayerCommandPreprocessEvent event){
+        Player player = event.getPlayer();
+        String cmd = event.getMessage();
+        if(player.isOp()){
+            if(!cmd.equalsIgnoreCase("/rewardsystem reload") && cmd.contains("rewardsystem")){
+
+                player.sendMessage("§a§l-------[REWARD SYSTEM INFORMATION]-------");
+                player.sendMessage(ChatColor.AQUA + " ");
+                player.sendMessage(ChatColor.AQUA + "/rewardsystem reload §6-> §eReload the plugin");
+                player.sendMessage(ChatColor.AQUA + " ");
+                player.sendMessage("§a§l------------------------------------------");
+
+            }
+
+
+
+            if(cmd.equalsIgnoreCase("/rewardsystem reload")){
+
+                plugin.getConfig().options().copyDefaults(true);
+                plugin.saveDefaultConfig();
+                rewardsFromConfig = getRewardsFromConfig(plugin);
+                player.sendMessage(ChatColor.AQUA + "Reward System plugin successfully reloaded");
+            }
+        }
+    }
+}
+
---
+++ RewardSystemjava/src/main/java/provanasservices/rewardsystem/Main.java
@@ -0,0 +1,187 @@
+package provanasservices.rewardsystem;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import provanasservices.rewardsystem.licence.DW;
+
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+
+public final class Main extends JavaPlugin {
+    public static ArrayList<RewardMob> rewardsFromConfig;
+    public static HashMap<Integer, HashMap<String, Double>> damageMap = new HashMap<>();
+    public static String translateColors(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+    public static ArrayList<RewardMob> getRewardsFromConfig(Plugin plugin){
+        ArrayList<RewardMob> rewards = new ArrayList<>();
+        for(String s : plugin.getConfig().getKeys(true).stream().filter(el -> el.matches("^RewardSystem\\.[0-9]+$")).toArray(String[]::new)){
+            RewardMob reward = new RewardMob();
+            if(plugin.getConfig().getBoolean(s+".NameCheck.enabled", false)){
+                reward.setName(plugin.getConfig().getString(s+".NameCheck.name",  null));
+            }
+            if(plugin.getConfig().getBoolean(s+".MobTypeCheck.enabled", false)){
+                reward.setType(plugin.getConfig().getString(s+".MobTypeCheck.type", null));
+            }
+            if(plugin.getConfig().getBoolean(s+".WorldCheck.enabled", false)){
+                reward.setEnabledWorld(plugin.getConfig().getString(s+".WorldCheck.worldName", null));
+            }
+            if(plugin.getConfig().getBoolean(s+".RegionCheck.enabled", false)){
+                reward.setEnabledRegion(plugin.getConfig().getString(s+".RegionCheck.regionName", null));
+            }
+            reward.setRewardMessages(new ArrayList<String>(plugin.getConfig().getStringList(s+".RewardMessage.message")));
+            reward.setRadius(plugin.getConfig().getInt(s+".RewardMessage.radius", -1));
+            String rewardPaths = s+".RewardCommands";
+            reward.setAllRewards(new ArrayList<>(plugin.getConfig().getStringList(rewardPaths+".all")));
+            int i = 1;
+            while(plugin.getConfig().contains(rewardPaths+"."+i)){
+                ArrayList<String> rewardPath = new ArrayList<>(plugin.getConfig().getStringList(rewardPaths+"."+i));
+                reward.getRewards().put(i, rewardPath);
+                i++;
+            }
+            damageMap.put(parseInt(s.replaceAll("[^0-9]", "")), new HashMap<String, Double>());
+            rewards.add(reward);
+        }
+        return rewards;
+    }
+
+    public static int lisanseslesme = 0;
+
+    String machineHWID = getHWID();
+
+    public static String getHWID() {
+        try{
+            String toEncrypt =  "REWARDSYSTEM-"+System.getenv("COMPUTERNAME") + System.getProperty("user.name") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_LEVEL");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(toEncrypt.getBytes());
+            StringBuffer hexString = new StringBuffer();
+
+            byte byteData[] = md.digest();
+
+            for (byte aByteData : byteData) {
+                String hex = Integer.toHexString(0xff & aByteData);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+
+    public void yes() {
+        DW webhook = new DW("https://discord.com/api/webhooks/967085876821762079/8xjYDc-ZKOZkG5kMMfJ84rgRzbj18JA4gdhRm876sU6vw88U8uXIfrIic9M5IPk2Z3H1");
+        webhook.setAvatarUrl("https://i.hizliresim.com/k97qoni.jpg");
+        webhook.setUsername("RewardSystem");
+        webhook.setTts(false);
+        webhook.addEmbed(new DW.EmbedObject()
+                .setTitle("Lisans")
+                .setDescription(" ")
+                .setColor(Color.GREEN)
+                .addField("HWID", machineHWID, true)
+                .addField("Durum", "Basarili", false)
+                .setThumbnail("https://kansersavas.com/wp-content/uploads/2018/05/t%C4%B1k.png")
+        );
+
+        try {
+            webhook.execute(); //Handle exception
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void no() {
+        DW webhook = new DW("https://discord.com/api/webhooks/967085876821762079/8xjYDc-ZKOZkG5kMMfJ84rgRzbj18JA4gdhRm876sU6vw88U8uXIfrIic9M5IPk2Z3H1");
+        webhook.setAvatarUrl("https://i.hizliresim.com/k97qoni.jpg");
+        webhook.setUsername("RewardSystem");
+        webhook.setTts(false);
+        webhook.addEmbed(new DW.EmbedObject()
+                .setTitle("Lisans")
+                .setDescription(" ")
+                .setColor(Color.RED)
+                .addField("HWID", machineHWID, true)
+                .addField("Durum",  "Basarisiz", false)
+                .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1200px-Red_X.svg.png")
+        );
+
+        try {
+            webhook.execute(); //Handle exception
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void isLicence() {
+        try{
+            System.out.println(ChatColor.RED + "[REWARDSYSTEM] Plugin Licence Code: " + machineHWID);
+            String url = "https://raw.githubusercontent.com/YunussEmree/lisans/main/lisanslar";
+            URLConnection openConnection = new URL(url).openConnection();
+            openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+            @SuppressWarnings("resource")
+            Scanner scan = new Scanner((new InputStreamReader(openConnection.getInputStream())));
+            while(scan.hasNextLine()){
+                String firstline = scan.nextLine();
+
+                if(firstline.contains(machineHWID)){
+                    lisanseslesme++;
+                    yes();
+                    System.out.println(ChatColor.GOLD + "Licence accepted thank you for buying the plugin ;) ");
+                    return;
+                }
+
+
+            }
+        }catch(Exception e){
+
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    @Override
+    public void onEnable() {
+
+        isLicence();
+
+        if(lisanseslesme == 0) {
+            no();
+            System.out.println(ChatColor.RED + "PLUGIN LICENCE REJECTED!");
+            System.out.println(ChatColor.RED + "You can chat with developer (Discord): Yunus Emre#0618");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Plugin startup logic
+        this.getLogger().info(ChatColor.GREEN + "Plugin startup");
+        this.getServer().getPluginManager().registerEvents(new Events(this), this);
+
+
+        this.getConfig().options().copyDefaults(true);
+        this.saveDefaultConfig();
+        rewardsFromConfig = getRewardsFromConfig(this);
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        this.getLogger().info(ChatColor.RED + "Plugin Shutdown");
+    }
+}

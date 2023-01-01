83e0fdd29213eb5bb5be35e8b30e64fe4b48a53d
MetuMortis <78408599+LiberaTeMetuMortis@users.noreply.github.com>
2023-01-01 14:20:08 +0300

    Avalon build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
 
     compileOnly(group="me.clip", name="placeholderapi", version="2.11.1")
-    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
     compileOnly("org.bstats:bstats-bukkit:1.7")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.4")
 
@@ -59,13 +59,13 @@ application {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "16"
+        jvmTarget = "11"
     }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 
